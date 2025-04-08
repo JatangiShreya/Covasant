124f9640e0b7030d1e97c7be324ed53c2c95bfbc
JatangiShreya <134074274+JatangiShreya@users.noreply.github.com>
2025-04-08 21:50:18 +0530

    Update Question3.py

--- Question3.py
+++ Question3.py
@@ -1,30 +1,24 @@
 import os.path
 import glob
 name=r"C:\Users\Shreya\check"
+max_size=0
+file_name=""
 def directory_list(name):
-    max_size=0
-    file_name=""
+    global max_size
+    global file_name
     size=0
     lst=glob.glob(f"{name}"+"\\*")
     for item in lst:
         if os.path.isdir(item):
-            direc_items=glob.glob(f"{item}"+"\\*")
-            for it in direc_items:
-                if os.path.isfile(it):
-                        size=os.path.getsize(f"{it}")
-                        if max_size<size:
-                            max_size=size
-                            file_name=file_name+os.path.basename(f"{it}")
-                else:
-                        directory_list(it)    
+            directory_list(item)    
         else:
             size=os.path.getsize(f"{item}")
             if max_size<size:
                 max_size=size
-                file_name=file_name+os.path.basename(f"{item}")
-    print(max_size)
-    print(file_name)
-directory_list(name)
+                file_name=file_name+os.path.basename(f"{item}") 
+    return max_size,file_name
+print(directory_list(name))
+
 
 
         
