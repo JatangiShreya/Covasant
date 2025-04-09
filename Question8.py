c96c88127830e9518e9e85c04e282c3faf3c8159
JatangiShreya <134074274+JatangiShreya@users.noreply.github.com>
2025-04-09 16:52:23 +0530

    Add files via upload

---
+++ Question8.py
@@ -0,0 +1,9 @@
+# DAY-5
+# Question-8:
+# Given a URL, download that and parse 
+# and download all links inside that page 
+    # in thread 
+    # in process 
+    # in asynchornous 
+    # ThreadPoolExecutor or ProcessPoolExecutor or asyncio
+    # BeautifulSoup for parsing html, requests for downloading
