0116a5a544fa0582fe360b97f606363bc4a696c8
JatangiShreya <134074274+JatangiShreya@users.noreply.github.com>
2025-04-09 21:40:24 +0530

    Update Question5.py

--- Question5.py
+++ Question5.py
@@ -5,5 +5,24 @@
 # c = a+b 
 # print(c) #Poly ( 1,0,1, 2,4,6)
 
+class Poly:
+    def __init__(self, *coeffs):
+        self.coeffs = list(coeffs)
+        
+    def __add__(self, other):
+        max_len = max(len(self.coeffs), len(other.coeffs))
+        result = [0] * max_len
+        for i in range(len(self.coeffs)):
+            result[i] += self.coeffs[i]
+        for i in range(len(other.coeffs)):
+            result[i] += other.coeffs[i]
+        return Poly(*result)
+    
+    def __repr__(self):
+        return "Poly(" + ", ".join(map(str, self.coeffs)) + ")"
 
+a = Poly(1, 2, 3) 
+b = Poly(1, 0, 1, 1, 2, 3)
+c = a + b 
+print(c)
 
