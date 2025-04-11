ce5be6d320109e756f2125b57d3aab4d77798611
JatangiShreya <134074274+JatangiShreya@users.noreply.github.com>
2025-04-11 22:17:03 +0530

    Add files via upload

---
+++ Question16.py
@@ -0,0 +1,6 @@
+# flask
+# Question-16:
+# Sharing of content 
+# @app.route("/updatefortoday", methods=['GET','POST'])#http://localhost:5000/updatefortoday
+# @app.route("/share", methods=['GET'])#http://localhost:5000/share
+# @app.route("/clearnotepadtxt", methods=['GET'])#http://localhost:5000/clearnotepadtxt
