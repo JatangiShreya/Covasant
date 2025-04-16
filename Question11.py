ba03c7e13ebc972b2b9c9d0084af77fd32706f7e
JatangiShreya <134074274+JatangiShreya@users.noreply.github.com>
2025-04-16 22:11:12 +0530

    Add files via upload

---
+++ Question11.py
@@ -0,0 +1,13 @@
+# Question-11:
+# Weather mock API 
+# format comes from below endpoint 
+    # Parse format, if xml, show xml, 
+    # if none, show json 
+    # Put all random temp 
+    # City name may change based on PATH param 
+    # (currently not implemented in demo api endpoint)
+
+# DO the same as 
+# http://notepadfromdas.pythonanywhere.com/w/mumbai?format=xml
+# http://notepadfromdas.pythonanywhere.com/w/mumbai
+
